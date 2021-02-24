eca641214b3d61f07cf9f59f2b04ba8beea3aeae
haoyong <yong.hao@turingsenseai.com>
2021-02-24 18:26:22 +0800

    init & user

---
+++ schema/user.d.ts
@@ -0,0 +1,38 @@
+/** 性别枚举 */
+export enum Sex {
+  UNKNOW = 0, // 未知
+  MALE = 1, // 男性
+  FEMAMLE = 2, // 女性
+}
+
+/** 管理员角色枚举 */
+export enum AdminRole {
+  ADMIN = 0,
+  OTHER = 1,
+}
+/** APP角色枚举 */
+export enum AppRole {
+  BLOGGER = 0,
+  OTHER = 1,
+}
+
+/** 用户 */
+export interface User {
+  id?: number;
+  userName: string;
+  realName: string;
+  sex: Sex;
+  age: number;
+  password: string;
+  tipQA: string;
+  email: string;
+  phone: string;
+  lastLogin: number;
+  registerTime: number;
+  updateTime: number;
+  adminRole?: AdminRole;
+  appRole?: AppRole;
+  token1?: string;
+  token2?: string;
+  token3?: string;
+}
